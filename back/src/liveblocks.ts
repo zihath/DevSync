a1d1dc9319f156dae55214242eda82562000c5d9
Sanjai <sanjai135rss@gmail.com>
2025-03-10 21:10:09 +0530

    changes for liveblocks.ts

--- back/src/liveblocks.ts
+++ back/src/liveblocks.ts
@@ -1,7 +1,7 @@
-import { Liveblocks } from "@liveblocks/node";
-const LIVEBLOCKS_SECRET_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
-const liveblocks = new Liveblocks({
-  secret: LIVEBLOCKS_SECRET_KEY || "",
-});
+// import { Liveblocks } from "@liveblocks/node";
+// const LIVEBLOCKS_SECRET_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
+// const liveblocks = new Liveblocks({
+//   secret: LIVEBLOCKS_SECRET_KEY || "",
+// });
 
-export default liveblocks;
+// export default liveblocks;
