e0af8883faec977e4713ecd267e193055800482c
zihath <133570794+zihath@users.noreply.github.com>
2025-03-15 18:08:55 +0530

    small change

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 import userRoutes from "./routes/userRoutes";
 import roomRoutes from "./routes/roomRoutes";
+import ProjectRoutes from "./routes/ProjectRoutes"
 
 dotenv.config();
 connectDB();
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", userRoutes);
 app.use("/api/rooms", roomRoutes);
+app.use("/api/projects" , ProjectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
